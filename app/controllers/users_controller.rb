1c52e606455facb0d32cac359eefd3ec2b450ef8
Dandibot <rakotoniaina.andry@gmail.com>
2016-09-19 15:56:36 +0800

    Change design Listing Show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def show
+  	return set_user
+  end
+
+
+    private
+  # The following method is called strong params see google
+  def user_params
+    params.require(:user).permit(:email)
+
+  end
+
+  def set_user
+    @user = User.find(params[:id]) # originally listing, but I changed with Listing
+ 
   end
 end
